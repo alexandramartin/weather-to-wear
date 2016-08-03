66b7f0d3882dbe0633b00535b78efdd5259253dc
Djuarex <tigerbear26@gmail.com>
2016-08-03 13:43:46 -0700

    edit index css, html, js, and where to shop html

--- js/index.js
+++ js/index.js
@@ -7,11 +7,14 @@ $(document).ready(function(){
 			// debugger;
 			var temperature = data.main.temp;
 			var description = data.weather[0].description
+			var iconCode = data.weather[0].icon
+			var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
 			//access the icon value inside of data
 			//concat the img url string with the icon value data
 			//append that to the weather-api div inside of and img tag with the img url string as the src of the image
 			$("#weather-api").append("<div class='weather' style='color: whitesmoke'>" + "Temperature is:" + temperature + "</div>"); 
 			$("#weather-api").append("<div class='weather' style='color: whitesmoke'>" + "It appears to be "  + description + "</div>");
+			$("#weather-api").append("<img src='" + iconUrl  + "'>");
 
 		}
 	})
